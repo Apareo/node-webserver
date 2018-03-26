2254ae942ecfc9a654943f39eaf369fe1de5a2be
Apareo <github@apareno.com>
2018-03-26 15:56:06 -0500

    Setup Start Script and Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var inMaint = true;
 
@@ -64,6 +65,6 @@ app.get('/bad', (req, resp) => {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
